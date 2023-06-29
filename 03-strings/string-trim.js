2de4a45ef9cdc182f9310a50c90451034eebda73
Ruben-Alvarez-Dev <info@ruben-alvarez.com>
2023-06-29 13:02:30 +0200

    feature: 05-numbers

---
+++ 03-strings/string-trim.js
@@ -0,0 +1,12 @@
+/*
+METODO TRIM: Elimina los espacios de PRINCIPIO Y FINAL
+*/
+
+const greeting = "    hello world    ";
+
+console.log(greeting.trim());
+
+// trinStart y trimEnd
+
+console.log(greeting.trimStart());
+console.log(greeting.trimEnd());
---
+++ 03-strings/string-valueof.js
@@ -0,0 +1,8 @@
+/*
+METODO VALUEOF: Saca el valor de un objeto, stringifica el valor de un objeto
+*/
+
+const stringObject = new String("foo");
+console.log(stringObject);
+
+console.log(stringObject.valueOf());
